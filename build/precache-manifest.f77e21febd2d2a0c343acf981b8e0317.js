430b6ef668e8cb3e479e94629de34b8f21696c8c
Dmitrii Kozhushko <d.a.kozhyshko@gmail.com>
2019-11-15 00:22:50 +0300

    First commit

---
+++ build/precache-manifest.f77e21febd2d2a0c343acf981b8e0317.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db85dd742af42f6fe28483649ed0ecd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca2a6f6bb17af69e75d8",
+    "url": "/static/css/main.5aa1c0a8.chunk.css"
+  },
+  {
+    "revision": "0695ffe9573228f4c98e",
+    "url": "/static/js/2.12f5cc7b.chunk.js"
+  },
+  {
+    "revision": "ca2a6f6bb17af69e75d8",
+    "url": "/static/js/main.c65ac0bd.chunk.js"
+  },
+  {
+    "revision": "82c197011109d48b6cee",
+    "url": "/static/js/runtime-main.e7ffe6e5.js"
+  },
+  {
+    "revision": "6215764657b41f00dd5bbe37b90a8eb2",
+    "url": "/static/media/check.62157646.svg"
+  },
+  {
+    "revision": "7ba727d8b592e88678bae0171a4f58e4",
+    "url": "/static/media/mail_f.7ba727d8.svg"
+  },
+  {
+    "revision": "c2258fb36da6f2ad0773eb355413c9d8",
+    "url": "/static/media/password.c2258fb3.svg"
+  },
+  {
+    "revision": "ed121d88e578b4c4523881b17201f5e2",
+    "url": "/static/media/password_f.ed121d88.svg"
+  }
+]);
